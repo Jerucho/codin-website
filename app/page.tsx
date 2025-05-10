a7ea854f298fa6033ea6bba2c8ba207cb127d330
Jeremias <jeremiasdavidamador@gmail.com>
2025-05-10 14:04:40 -0500

    Eliminar el componente Navbar de la página principal

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,12 @@ import { Banner } from "./Components/Banner";
 import { CallToAction } from "./Components/CallToAction/CallToAction";
 import { OurCustomers } from "./Components/Customers/OurCustomers";
 import { Footer } from "./Components/Footer/Footer";
-import { Navbar } from "./Components/Navbar";
 import { OurProcess } from "./Components/Process/OurProcess";
 import { ProyectsSection } from "./Components/Proyects/ProyectsSection";
 import { Services } from "./Components/Services/Services";
 export default function Home() {
   return (
     <>
-      <Navbar />
       <Banner />
       <Services />
       <ProyectsSection />
